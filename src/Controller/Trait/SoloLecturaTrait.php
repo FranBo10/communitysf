592228709b02c6c0d1743b09bfe83330a6aa0270
FranBo10 <franbo10pro@gmail.com>
2023-09-28 14:19:52 +0200

    Trait

--- src/Controller/Trait/SoloLecturaTrait.php
+++ src/Controller/Trait/SoloLecturaTrait.php
@@ -2,18 +2,18 @@
 
 // namespace App\Controller\Admin\Trait;
 
-// use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-// use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
-// use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 
-// trait SoloLecturaTrait
-// {
-//     public function configureActions(Actions $actions): Actions
+trait SoloLecturaTrait
+{
+    public function configureActions(Actions $actions): Actions
 
-//     {
-//         $actions
-//             ->disable(Action::NEW, Action::EDIT, Action::DELETE)
-//             ->add(Crud::PAGE_INDEX, Action::DETAIL);
-//         return $actions;
-//     }
-// }
+    {
+        $actions
+            ->disable(Action::NEW, Action::EDIT, Action::DELETE)
+            ->add(Crud::PAGE_INDEX, Action::DETAIL);
+        return $actions;
+    }
+}
